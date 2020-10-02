7a401e1f8bb3358033fe4edd49875657a1b34736
Baiyun-u-smartAI <45277409+Baiyun-u-smartAI@users.noreply.github.com>
2020-10-02 22:17:56 +0800

    Add files via upload

--- boost_tcp/main.cpp
+++ boost_tcp/main.cpp
@@ -1,104 +1,53 @@
-#include <bits/stdc++.h>
 #include <boost/asio.hpp>
-#ifdef IS_SERVER
-// BoostTcpServer.cpp : 定义控制台应用程序的入口点。
-//
+#include <iostream>
+
+using namespace boost;
 
 
+// DNS resolver  (get ip from ping)
 
-using namespace boost::asio;
 
-int main(int argc, char* argv[])
+int main()
 {
-#if 1
-    // 所有asio类都需要io_service对象
-    io_service iosev;
-    ip::tcp::acceptor acceptor(iosev,
-                               ip::tcp::endpoint(ip::tcp::v4(), 1000));
+    // Step 1. Assume that the client application has already
+    // obtained the DNS name and protocol port number and
+    // represented them as strings.
+    std::string host = "baidu.com";
+    std::string port_num = "80";
 
-    for(;;)
+    // Step 2.
+    asio::io_service ios;
+
+    // Step 3. Creating a query.
+    asio::ip::tcp::resolver::query resolver_query(host,
+                                                  port_num, asio::ip::tcp::resolver::query::numeric_service);
+
+    // Step 4. Creating a resolver.
+    asio::ip::tcp::resolver resolver(ios);
+
+    // Used to store information about error that happens
+    // during the resolution process.
+    boost::system::error_code ec;
+
+    // Step 5.
+    asio::ip::tcp::resolver::iterator it =
+            resolver.resolve(resolver_query, ec);
+
+    asio::ip::tcp::resolver::iterator end;
+    while (it != end)
     {
-        // socket对象
-        ip::tcp::socket socket(iosev);
-
-        // 等待直到客户端连接进来
-        acceptor.accept(socket);
-        // 显示连接进来的客户端
-
-        std::cout << socket.remote_endpoint().address() << std::endl;
-
-
-        // 向客户端发送message
-        boost::system::error_code ec;
-        std::string message;
-        while (std::cin>>message) {
-            socket.write_some(buffer(message), ec);
-            if (*(message.end()-1)=='@') break;
-        }
-
-        // 如果出错，打印出错信息
-        if(ec)
-        {
-            std::cout <<
-                      boost::system::system_error(ec).what() << std::endl;
-            break;
-        }
-        // 与当前客户交互完成后循环继续等待下一客户连接
+        asio::ip::tcp::endpoint ep = *it++;
+        std::cout << ep << std::endl;
     }
-#endif
+    // Handling errors if any.
+    if (bool(ec) != 0) {
+        // Failed to resolve the DNS name. Breaking execution.
+        std::cout << "Failed to resolve a DNS name."
+                  << "Error code = " << ec.value()
+                  << ". Message = " << ec.message();
+
+        return ec.value();
+    }
 
     return 0;
 }
-#endif
-
-#ifdef IS_CLIENT
-//code of client
-#include <iostream>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-using namespace std;
-using boost::asio::ip::tcp;
-
-int main(int argc, char* argv[])
-{
-    try
-    {
-        //(1)通过tcp::socket类定义一个tcp client对象socket
-        boost::asio::io_service io;
-        tcp::socket socket(io);
-
-        //(2)通过connect函数连接服务器，打开socket连接。
-        tcp::endpoint end_point(boost::asio::ip::address::from_string("127.0.0.1"), 1000);
-        socket.connect(end_point);
-
-        for (;;)
-        {
-            boost::array<char, 128> buf;
-            boost::system::error_code error;
-
-            //(3)通过read_some函数来读数据
-            size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-            if (error == boost::asio::error::eof)
-            {
-                break;    //connection closed cleadly by peer
-            }
-            else if (error)
-            {
-                throw boost::system::system_error(error);    //some other error
-            }
-
-            cout.write(buf.data(), len);
-            cout<<"\n";
-        }
-    }
-    catch (std::exception& e)
-    {
-        cout<<e.what()<<endl;
-    }
-    system("pause");
-}
-
-
-
-#endif
---
+++ boost_tcp/tcp_sync.h
@@ -0,0 +1,107 @@
+//
+// Created by pc002 on 2020/10/2.
+//
+
+#ifndef TCP_IP_CLIENT_TCP_SYNC_H
+#define TCP_IP_CLIENT_TCP_SYNC_H
+#include <bits/stdc++.h>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+using namespace std;
+using boost::asio::ip::tcp;
+using namespace boost::asio;
+
+int server(int argc, char* argv[])
+{
+#if 1
+    // 所有asio类都需要io_service对象
+    io_service iosev;
+    ip::tcp::acceptor acceptor(iosev,
+                               ip::tcp::endpoint(ip::tcp::v4(), 1000));
+
+    for(;;)
+    {
+        // socket对象
+        ip::tcp::socket socket(iosev);
+
+        // 等待直到客户端连接进来
+        acceptor.accept(socket);
+        // 显示连接进来的客户端
+        std::cout << "已连接上地址：";
+        std::cout << socket.remote_endpoint().address() << std::endl;
+        std::cout << "请输入发送内容，如果以@结尾则终止本次通讯："<< std::endl;
+
+        // 向客户端发送message
+        boost::system::error_code ec;
+        std::string message;
+        while (std::cin>>message) {
+            socket.write_some(buffer(message), ec);
+            if (*(message.end()-1)=='@') break;
+        }
+
+        // 如果出错，打印出错信息
+        if(ec)
+        {
+            std::cout <<
+                      boost::system::system_error(ec).what() << std::endl;
+            break;
+        }
+        // 与当前客户交互完成后循环继续等待下一客户连接
+    }
+#endif
+
+    return 0;
+}
+
+int server(int argc, char* argv[])
+{
+    try
+    {
+        //(1)通过tcp::socket类定义一个tcp client对象socket
+        boost::asio::io_service io;
+        tcp::socket socket(io);
+
+        //(2)通过connect函数连接服务器，打开socket连接。
+        tcp::endpoint end_point(boost::asio::ip::address::from_string("39.108.140.215"), 32123);
+        socket.connect(end_point);
+
+        for (;;)
+        {
+            boost::array<char, 128> buf;
+            boost::system::error_code error;
+
+            //(3)通过read_some函数来读数据
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof)
+            {
+                break;    //connection closed cleadly by peer
+            }
+            else if (error)
+            {
+                throw boost::system::system_error(error);    //some other error
+            }
+
+            cout.write(buf.data(), len);
+            cout<<"\n";
+        }
+    }
+    catch (std::exception& e)
+    {
+        cout<<e.what()<<endl;
+    }
+    system("pause");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //TCP_IP_CLIENT_TCP_SYNC_H
